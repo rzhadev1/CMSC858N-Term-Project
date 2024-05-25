4e520c862ba6fd7fafa1c807eb7ebdcd47e3f417
Richard Z <richardz@RichardsMBP317.fios-router.home>
2024-05-24 21:19:03 -0400

    don't use tokens

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -7,97 +7,80 @@
 #include <parlay/io.h>
 #include "parser.h"
 
-
-// using adj_mat = parlay::sequence<parlay::sequence<int>>;
-
-// Read the dimacs file to a parlay adjacency list representation
 FlowInstance readDimacsToFlowInstance(const std::string& filename) {
+
+		parlay::internal::timer timer("time");
     auto str = parlay::file_map(filename);
-    auto lines = parlay::tokens(str, [&] (char c) {return c == '\n';});  // split the line on newlines into sequence of std::strings
-    
+    timer.next("file map"); 
+		auto lines = parlay::tokens(str, [&] (char c) {return c == '\n';});  // split the line on newlines into sequence of std::strings
+    timer.next("tokens");
     vertex_id S = -1, T = -1; // the source and sink 
     int n = 0, m = 0;  // number of vertices, number of edges
     
     weighted_graph dimacs_adj;
 
-    using edge_key = std::pair<vertex_id, vertex_id>; // map the (u,v) edge to its capacity
-    std::map<edge_key, weight> edge_map; 
+    std::map<edge, weight> edge_map; 
 
-    // parse all the comments and problem statement line from dimacs
-    for(const auto& line : lines) {
+    for(const auto& pline : lines) {
+				const char* line = pline.data();
         char type = line[0];
-        auto tokens = parlay::tokens(line, [] (char c) {return c == ' ';});
-
-        if(type == 'c') { 
-            std::cout << line << std::endl;  // comment line
-        }
+				
+				// slow!
+        // auto tokens = parlay::tokens(line, [] (char c) {return c == ' ';});
+        if(type == 'c') {}
 
         else if (type == 'p') {  // problem statement line contains n,m
-            n = parlay::chars_to_int(tokens[2]);
-            m = parlay::chars_to_int(tokens[3]);
-            
-            std::cout << "num vertices: " << n << std::endl; 
-            std::cout << "num edges: " << m << std::endl; 
-            
-            // for each vertex, create an empty sequence of edges
-            dimacs_adj = parlay::tabulate<edges>(n, [&] (size_t i) {return edges();});
+					std::sscanf(line, "p %*3s %d %d", &n, &m);	
         }
 
         else if(type == 'n') { // specify source/sink
-            if (tokens[2][0] == 's') {
-                S = parlay::chars_to_int(tokens[1]) - 1;
-                std::cout << "source: " << S << std::endl;
-            }
-            else if (tokens[2][0] == 't') {
-                T = parlay::chars_to_int(tokens[1]) - 1;
-                std::cout << "sink: " << T << std::endl; 
-            }
+					int id; 
+					char which;
+        	std::sscanf(line, "n %d %c", &id, &which);
 
-            else {
-                std::cout << "invalid node descriptor line" << std::endl;
-            }
-        }
+					if(which == 's') {
+						S = id; 
+					}
+					else if(which == 't') {
+						T = id;
+					}
+				}
 
         else if(type == 'a') {
-            // we must have sequences initialized to get here
-            
-            vertex_id src = parlay::chars_to_int(tokens[1]) - 1;
-            vertex_id dst = parlay::chars_to_int(tokens[2]) - 1;
+					vertex_id src; 
+					vertex_id dst; 
+					weight cap;
+					std::sscanf(line, "a %d %d %d", &src, &dst, &cap);
 
-            weight cap = static_cast<weight>(parlay::chars_to_int(tokens[3]));
+					// 1 indexed -> 0 indexed
+					src--; 
+					dst--;
 
-            // this line specifies a (u,v) edge. we only update capacities for 
-            // (u,v) edges. all reverse edges are assumed to have 0 capacity until 
-            // we see the reverse edge as (u,v)
-
-            // is edge (u,v) not in the graph?
-            if(edge_map.find({src, dst}) == edge_map.end()) {
-                edge_map.insert({{src, dst}, cap}); // insert into edge map
-            }
-
-            // in the graph, update its capacity
-            else {
-                edge_map.at({src, dst}) = cap;
-            }
-
-            // is edge (v,u) not in the graph?
-            if(edge_map.find({dst, src}) == edge_map.end()) { 
-                edge_map.insert({{dst, src}, 0}); // insert into edge map with 0 capacity
-            }
-        }
+					auto e1 = edge_map.insert({{src, dst}, cap});
+					auto e2 = edge_map.insert({{dst, src}, 0});
+					
+					// if the edge already exists, 
+					// then set the capacity instead
+					if(e1.second == false) {
+						e1.first -> second = cap;
+					}
+				}
 
         else {
             std::cout << "invalid line!: " << line << std::endl; 
         }
     }
-		
+
+		timer.next("parse"); 
+		dimacs_adj = parlay::tabulate<edges>(n, [&] (size_t i) {return edges();});
     for(auto& edge : edge_map) {
         vertex_id src = std::get<0>(edge.first); 
         vertex_id dst = std::get<1>(edge.first);
-        weight cap = edge.second;
-        
+				weight cap = edge.second;
+				
         dimacs_adj[src].push_back({dst, cap});
     }
+		timer.next("convert graph");
 
     FlowInstance flow;
     flow.n = n; 
